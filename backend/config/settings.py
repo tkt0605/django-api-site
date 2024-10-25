da63bbf78a157e06677765c490c2014bb32f2497
tkt0605 <takato.komada2005@outlook.jp>
2024-10-25 22:37:29 +0900

    staticfilesの作成

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -147,7 +147,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # collectstatic で収集される場所
+
+# 開発中の静的ファイルを配置する場所
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Default primary key field type

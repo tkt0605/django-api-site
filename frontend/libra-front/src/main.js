f8de71ca5aaa4ede05f7056a94f477edaa1cea96
tkt0605 <takato.komada2005@outlook.jp>
2024-10-27 05:25:30 +0900

    ヂィレクトリ移動

--- frontend/libra-front/src/main.js
+++ frontend/libra-front/src/main.js
@@ -3,6 +3,6 @@ import { createApp } from 'vue';  // Vue 3用のインポート
 import router from './router';  // ルーターをインポート
 import App from './App.vue';
 
-const app = createApp(App);  // Vueインスタンスを作
+const app = createApp(App);  // Vueインスタンスを作る・ここがないとテンプレート表示出来ないので注意
 app.use(router);
 app.mount('#app');
--- frontend/libra-front/src/router/index.js
+++ frontend/libra-front/src/router/index.js
@@ -1,18 +1,14 @@
 import {createRouter, createWebHistory} from "vue-router";
-import UserLogin from '@/views/Login.vue';
-import UserRegister from '@/views/Register.vue';
-import HomeIndex from "@/views/Index.vue";
-import AccountProfile from "@/views/Account.vue";
-import AuthLayout from "@/layout/AuthLayout.vue";
-import DefaultLayout from "@/layout/DefaultLayout.vue";
+import UserLogin from '@/components/views/Login.vue';
+import UserRegister from '@/components/views/Register.vue';
+import HomeIndex from "@/components/views/Index.vue";
+import AccountProfile from "@/components/views/Account.vue";
+import AuthLayout from "@/components/layout/AuthLayout.vue";
+import DefaultLayout from "@/components/layout/DefaultLayout.vue";
 const routes = [
-    // {path: "/accounts/login", name: 'Login', component: UserLogin},
-    // {path: "/accounts/register", name: 'register', component: UserRegister},
-    // {path: "/", name: 'Index', component: HomeIndex},
-    // {path: "/profile", name: "account", component: AccountProfile},
     {
         path: '/',
-        component: DefaultLayout, // ログイン専用レイアウト
+        component: DefaultLayout, // デフォルトページ専用のレイアウト
         children: [
             {path: "", name: 'Index', component: HomeIndex},
             {path: "profile", name: "account", component: AccountProfile},

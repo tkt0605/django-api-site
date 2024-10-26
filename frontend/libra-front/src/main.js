a84ac8e07995e2d60daa99ce47478182b19551c5
tkt0605 <takato.komada2005@outlook.jp>
2024-10-26 22:06:08 +0900

    基に戻した。

--- frontend/libra-front/src/main.js
+++ frontend/libra-front/src/main.js
@@ -1,12 +1,8 @@
 // import Vue from 'vue'
 import { createApp } from 'vue';  // Vue 3用のインポート
 import App from './App.vue';
-import Account from './Account.Vue'
 import router from './router';  // ルーターをインポート
 
-const app = createApp(App);  // Vueインスタンスを作成
-const account = createApp(Account)
+const app = createApp(App);  // Vueインスタンスを作
 app.use(router);
 app.mount('#app');
-account.use(router);
-account.mount("#account")

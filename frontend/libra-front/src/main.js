aefb70538be52823533e571a9e317762968ea1f9
tkt0605 <takato.komada2005@outlook.jp>
2024-10-26 23:00:23 +0900

    一回、viewsフォルダーとlayoutフォルダーにファイル移動

--- frontend/libra-front/src/main.js
+++ frontend/libra-front/src/main.js
@@ -1,8 +1,8 @@
 // import Vue from 'vue'
 import { createApp } from 'vue';  // Vue 3用のインポート
-import App from './App.vue';
+// import App from './App.vue';
 import router from './router';  // ルーターをインポート
 
-const app = createApp(App);  // Vueインスタンスを作
+const app = createApp({});  // Vueインスタンスを作
 app.use(router);
 app.mount('#app');
--- frontend/libra-front/src/router/index.js
+++ frontend/libra-front/src/router/index.js
@@ -1,14 +1,23 @@
 import {createRouter, createWebHistory} from "vue-router";
-import UserLogin from '@/components/Login.vue';
-import UserRegister from '@/components/Register.vue';
-import HomeIndex from "@/components/Index.vue";
-import AccountProfile from "@/components/Account.vue";
-import AuthLayout from "@/components/AuthLayout.vue";
+import UserLogin from '../views/Login.vue';
+import UserRegister from '../views/Register.vue';
+import HomeIndex from "../views/Index.vue";
+import AccountProfile from "../views/Account.vue";
+import AuthLayout from "../layout/AuthLayout.vue";
+import DefaultLayout from "../layout/DefaultLayout.vue";
 const routes = [
-    {path: "/", name: 'Index', component: HomeIndex},
     // {path: "/accounts/login", name: 'Login', component: UserLogin},
     // {path: "/accounts/register", name: 'register', component: UserRegister},
-    {path: "/profile", name: "account", component: AccountProfile},
+    // {path: "/", name: 'Index', component: HomeIndex},
+    // {path: "/profile", name: "account", component: AccountProfile},
+    {
+        path: '/',
+        component: DefaultLayout, // ログイン専用レイアウト
+        children: [
+            {path: "/", name: 'Index', component: HomeIndex},
+            {path: "/profile", name: "account", component: AccountProfile},
+        ],
+      },
     {
         path: '/accounts',
         component: AuthLayout, // ログイン専用レイアウト

a333f74de4a512ae0a17217599c638cdbedf4183
tkt0605 <takato.komada2005@outlook.jp>
2024-10-26 22:01:37 +0900

    Account.VueとLogin・Registerページのベース作成

--- frontend/libra-front/src/main.js
+++ frontend/libra-front/src/main.js
@@ -1,8 +1,12 @@
 // import Vue from 'vue'
 import { createApp } from 'vue';  // Vue 3用のインポート
 import App from './App.vue';
+import Account from './Account.Vue'
 import router from './router';  // ルーターをインポート
 
 const app = createApp(App);  // Vueインスタンスを作成
+const account = createApp(Account)
 app.use(router);
 app.mount('#app');
+account.use(router);
+account.mount("#account")

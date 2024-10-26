828c65a0c60f563361fff2b6dceb5b5920728849
tkt0605 <takato.komada2005@outlook.jp>
2024-10-26 22:08:17 +0900

    Account.Vueをcomponentsヂィレクトリに移動

--- frontend/libra-front/src/router/index.js
+++ frontend/libra-front/src/router/index.js
@@ -2,11 +2,13 @@ import {createRouter, createWebHistory} from "vue-router";
 import UserLogin from '@/components/Login.vue';
 import UserRegister from '@/components/Register.vue';
 import HomeIndex from "@/components/Index.vue";
+import AccountProfile from "@/components/"
 
 const routes = [
     {path: "/", name: 'Index', component: HomeIndex},
     {path: "/accounts/login", name: 'Login', component: UserLogin},
     {path: "/accounts/register", name: 'register', component: UserRegister},
+    {path: "/profile", name: "account", component: }
 ];
 
 const router = createRouter({
